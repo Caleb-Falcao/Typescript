827d1ba23f90da2a129cf6a36882f9a8882f8525
Caleb-Falcao <calebfalcao1@gmail.com>
2024-06-04 12:32:09 -0300

    asdasd

---
+++ 001 - instalação/a.js
@@ -0,0 +1,7 @@
+//typescript
+// npm install -g typescrpt
+// tsc --init
+//Plugin: Code Runner
+// npm i -g ts-node
+// ctrl + alt + n
+console.log("npm i -g typescript");
---
+++ 001 - instalação/a.ts
@@ -0,0 +1,9 @@
+//typescript
+// npm install -g typescrpt
+// tsc --init
+
+//Plugin: Code Runner
+// npm i -g ts-node
+// ctrl + alt + n
+
+console.log("npm i -g typescript");
